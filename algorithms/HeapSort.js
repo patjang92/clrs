a68622069242e7d2c2da75ec8b50f1ee1f200bbd
Patrick Jang <patrick.jang@dynamo.works>
2019-01-04 18:08:59 -0500

    do heapsort in place without building Heap data structure

--- algorithms/HeapSort.js
+++ algorithms/HeapSort.js
@@ -1,7 +1,44 @@
-import Heap from '../data-structures/MaxHeap'
-
 export default function HeapSort(A) {
-	const heap = new Heap(A);
-	heap.sort();
-	return heap.toArray();
+	buildMaxHeap(A);
+	sortMaxHeap(A);
+	return A;
+}
+
+function maxHeapify(A, rootIndex, heapSize = A.length){
+	let leftIndex = (2 * rootIndex) + 1;
+	let rightIndex = (2 * rootIndex) + 2;
+	let largestIndex = rootIndex;
+
+	if (leftIndex < heapSize && A[leftIndex] > A[largestIndex]) {
+		largestIndex = leftIndex;
+	}
+
+	if (rightIndex < heapSize && A[rightIndex] > A[largestIndex]) {
+		largestIndex = rightIndex;
+	}
+
+	if (largestIndex != rootIndex) {
+		const tmp = A[rootIndex];
+		A[rootIndex] = A[largestIndex];
+		A[largestIndex] = tmp;
+
+		maxHeapify(A, largestIndex, heapSize);
+	}
+}
+
+function buildMaxHeap(A) {
+	let heapSize = A.length;
+	for (let i = Math.floor((heapSize - 1) / 2); i >= 0; i--) {
+		maxHeapify(A, i);
+	}
+}
+
+function sortMaxHeap(A) {
+	for (let i = A.length - 1; i >= 1; i--) {
+		const tmp = A[i];
+		A[i] = A[0];
+		A[0] = tmp;
+
+		maxHeapify(A, 0, i);
+	}
 }
