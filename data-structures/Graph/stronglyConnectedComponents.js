592f096518146ce8c539740b0c61fb7e7e3c0de7
Patrick Jang <patrick.jang@dynamo.works>
2019-02-01 11:08:35 -0500

    git add *

--- data-structures/Graph/stronglyConnectedComponents.js
+++ data-structures/Graph/stronglyConnectedComponents.js
@@ -5,20 +5,13 @@ import Stack from '../Stack/Stack';
 export default function stronglyConnectedComponents(graph) {
 
   let verticesByDecreasingFinishTime = getVerticesByDecreasingFinishTime(graph);
-  // compute Gt
   graph.reverse();
 
   return getStronglyConnectedComponents(graph, verticesByDecreasingFinishTime);
 }
 
 function getVerticesByDecreasingFinishTime(graph) {
-  // call DFS(G) to compute finishing times u.f for each vertex u
-
-  let time = 0;
-  // const startTimes = {};  
-  const finishTimes = {};
   const seen = {};
-
   const stack = new Stack();
 
   const callbacks = {
@@ -29,8 +22,6 @@ function getVerticesByDecreasingFinishTime(graph) {
       seen[currentVertex.getKey()] = true;
     },
     leaveVertex: ({ currentVertex }) => {
-      time++;
-      finishTimes[currentVertex.getKey()] = time;
       stack.push(currentVertex);
     }
   };
@@ -41,67 +32,34 @@ function getVerticesByDecreasingFinishTime(graph) {
     }
   })
 
-  // console.log("finishTimes = ", finishTimes);
-
   return stack;
-
 }
 
-function getStronglyConnectedComponents(graph, stack) {
-  // call DFS(Gt) but in the main loop of DFS< consider the vertices in order of decreasing u.f (as computed in line 1)
-  // output the vertices of each tree in the depth-first forest formed in line 3 as a separate strongly connected component
-  let time = 0;
-  // const startTimes = {};  
-  const finishTimes = {};
+function getStronglyConnectedComponents(graph, vertexStack) {
+
   const seen = {};
-
-  let sccSets = [];
+  const sccSets = [];
   let sccSet = [];
-
-
-  const enterVertexFactory = ((sccSet) => {
-    // let sccSet = [];
-    console.log("creating new enterVertex function");
-    return ({ currentVertex }) => {
-      console.log(`entering vertex ${currentVertex.getKey()}`)
-      seen[currentVertex.getKey()] = true;
-      sccSet.push(currentVertex);
-      console.log('pushed to sccSet, now looks like: ', sccSet.map(v => v.getKey()))
-      // console.log('sccSet :', sccSet);
-    }
-  });
 
   const callbacks = {
     allowTraversal: ({ nextVertex }) => {
-      console.log('considering traveling to nextVertex: ', nextVertex.getKey());
-      console.log('seen this vertex? ', seen.hasOwnProperty(nextVertex.getKey()));
       return !seen.hasOwnProperty(nextVertex.getKey());
     },
     enterVertex: ({ currentVertex }) => {
       seen[currentVertex.getKey()] = true;
       sccSet.push(currentVertex);
-      console.log('sccSet :', sccSet);
-    },
-    leaveVertex: ({ currentVertex }) => {
     }
   };
 
-  while(!stack.isEmpty()) {
-    let vertex = stack.pop();
-    console.log("stack.pop = ", vertex.getKey());
+  while(!vertexStack.isEmpty()) {
+    let vertex = vertexStack.pop();
     if (!seen.hasOwnProperty(vertex.getKey())) {
-      console.log(`${vertex.getKey()} has not been seen yet, doing dfs`);
-
-      // callbacks.enterVertex = enterVertexFactory(sccSet);
       sccSet = [];
       depthFirstSearch(graph, vertex, callbacks);
-
-      console.log("dfs finished, sccSet = ", sccSet.map(v => v.getKey()))
       sccSets.push(sccSet);
     }      
   }
 
-  console.log('sccSets :', sccSets.map((set) => set.map(v => v.getKey())));
   return sccSets;
 }
  
