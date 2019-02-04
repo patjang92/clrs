be8eac61f7c47119a9f1c4f976c7964e56a7657c
Patrick Jang <patrick.jang@dynamo.works>
2019-02-04 18:51:32 -0500

    update quicksort

--- algorithms/sorts/QuickSort.js
+++ algorithms/sorts/QuickSort.js
@@ -1,35 +1,114 @@
-export default function QuickSort(A, start, end) {
-  if (end <= start || start >= end)  return A;
-  let mid = partition(A, start, end);
-  QuickSort(A, start, mid-1);
-  QuickSort(A, mid+1, end);
-  return A;
-}
+import Sort from './Sort';
 
-function partition(A, start, end) {
+export default class QuickSortInPlace extends Sort {
+  /** Sorting in place avoids unnecessary use of additional memory, but modifies input array.
+   *
+   * This process is difficult to describe, but much clearer with a visualization:
+   * @see: http://www.algomation.com/algorithm/quick-sort-visualization
+   *
+   * @param {*[]} originalArray - Not sorted array.
+   * @param {number} inputLowIndex
+   * @param {number} inputHighIndex
+   * @param {boolean} recursiveCall
+   * @return {*[]} - Sorted array.
+   */
+  sort(
+    originalArray,
+    inputLowIndex = 0,
+    inputHighIndex = originalArray.length - 1,
+    recursiveCall = false,
+  ) {
+    // Copies array on initial call, and then sorts in place.
+    const array = recursiveCall ? originalArray : [...originalArray];
 
-  // randomized variant
-  const randomIndex = Math.floor(Math.random() * (end - start + 1)) + start;
-  swap(A, randomIndex, end);
-  
-  const pivot = A[end];
-  let i = start - 1;
+    /**
+     * The partitionArray() operates on the subarray between lowIndex and highIndex, inclusive.
+     * It arbitrarily chooses the last element in the subarray as the pivot.
+     * Then, it partially sorts the subarray into elements than are less than the pivot,
+     * and elements that are greater than or equal to the pivot.
+     * Each time partitionArray() is executed, the pivot element is in its final sorted position.
+     *
+     * @param {number} lowIndex
+     * @param {number} highIndex
+     * @return {number}
+     */
+    const partitionArray = (lowIndex, highIndex) => {
+      /**
+       * Swaps two elements in array.
+       * @param {number} leftIndex
+       * @param {number} rightIndex
+       */
+      const swap = (leftIndex, rightIndex) => {
+        const temp = array[leftIndex];
+        array[leftIndex] = array[rightIndex];
+        array[rightIndex] = temp;
+      };
 
-  // only iterate to end-1, as end is out pivot
-  for (let j = start; j < end; j++) {
-    if (A[j] < pivot) {
-      i++;
-      swap(A, i, j);
+      const pivot = array[highIndex];
+      // visitingCallback is used for time-complexity analysis.
+      this.callbacks.visitingCallback(array[pivot]);
+
+      let partitionIndex = lowIndex;
+      for (let currentIndex = lowIndex; currentIndex < highIndex; currentIndex += 1) {
+        if (this.comparator.lessThan(array[currentIndex], pivot)) {
+          swap(partitionIndex, currentIndex);
+          partitionIndex += 1;
+        }
+      }
+
+      // The element at the partitionIndex is guaranteed to be greater than or equal to pivot.
+      // All elements to the left of partitionIndex are guaranteed to be less than pivot.
+      // Swapping the pivot with the partitionIndex therefore places the pivot in its
+      // final sorted position.
+      swap(partitionIndex, highIndex);
+
+      return partitionIndex;
+    };
+
+    // Base case is when low and high converge.
+    if (inputLowIndex < inputHighIndex) {
+      const partitionIndex = partitionArray(inputLowIndex, inputHighIndex);
+      const RECURSIVE_CALL = true;
+      this.sort(array, inputLowIndex, partitionIndex - 1, RECURSIVE_CALL);
+      this.sort(array, partitionIndex + 1, inputHighIndex, RECURSIVE_CALL);
     }
+
+    return array;
   }
-  i++;
-  swap(A, i, end);
-
-  return i;
 }
 
-function swap(A, i, j) {
-  const tmp = A[i];
-  A[i] = A[j];
-  A[j] = tmp;
-}
+// export default function QuickSort(A, start, end) {
+//   if (end <= start || start >= end)  return A;
+//   let mid = partition(A, start, end);
+//   QuickSort(A, start, mid-1);
+//   QuickSort(A, mid+1, end);
+//   return A;
+// }
+
+// function partition(A, start, end) {
+
+//   // randomized variant
+//   const randomIndex = Math.floor(Math.random() * (end - start + 1)) + start;
+//   swap(A, randomIndex, end);
+  
+//   const pivot = A[end];
+//   let i = start - 1;
+
+//   // only iterate to end-1, as end is out pivot
+//   for (let j = start; j < end; j++) {
+//     if (A[j] < pivot) {
+//       i++;
+//       swap(A, i, j);
+//     }
+//   }
+//   i++;
+//   swap(A, i, end);
+
+//   return i;
+// }
+
+// function swap(A, i, j) {
+//   const tmp = A[i];
+//   A[i] = A[j];
+//   A[j] = tmp;
+// }
--- algorithms/sorts/QuickSort.test.js
+++ algorithms/sorts/QuickSort.test.js
@@ -1,40 +1,44 @@
 // TODO: make test suite
-import sort from './QuickSort';
+import QuickSort from './QuickSort';
 // const insertionSort = require('./insertion-sort');
+describe('QuickSort', () => {
 
-test('sorts input [1] to output [1]', () => {
-	expect(sort([1], 0, 0)).toEqual([1]);
+	const sorter = new QuickSort();
+	test('sorts input [1] to output [1]', () => {
+		expect(sorter.sort([1], 0, 0)).toEqual([1]);
+	})
+	
+	test('sorts input [1, 2] to output [1, 2]', () => {
+		expect(sorter.sort([1, 2], 0, 1)).toEqual([1, 2]);
+	})
+	
+	test('sorts input [2, 1] to output [1, 2]', () => {
+		expect(sorter.sort([2, 1], 0, 1)).toEqual([1, 2]);
+	})
+	
+	test('sorts input [3, 2, 1] to output [1, 2, 3]', () => {
+		expect(sorter.sort([3, 2, 1], 0, 2)).toEqual([1, 2, 3]);
+	})
+	
+	test('sorts input [1, 2, 3] to output [1, 2, 3]', () => {
+		expect(sorter.sort([1, 2, 3], 0, 2)).toEqual([1, 2, 3]);
+	})
+	
+	test('sorts input [1, 2, 3, 4] to output [1, 2, 3, 4]', () => {
+		expect(sorter.sort([1, 2, 3, 4], 0, 3)).toEqual([1, 2, 3, 4]);
+	})
+	
+	test('sorts input [2, 4, 1, 3] to output [1, 2, 3, 4]', () => {
+		expect(sorter.sort([2, 4, 1, 3], 0, 3)).toEqual([1, 2, 3, 4]);
+	})
+	
+	test('sorts input [4, 3, 2, 1] to output [1, 2, 3, 4]', () => {
+		expect(sorter.sort([4, 3, 2, 1], 0, 3)).toEqual([1, 2, 3, 4]);
+	})
+	
+	test('sorts input [8, 4, 6, 2, 3, 5, 7, 1 to output [1, 2, 3, 4, 5, 6, 7, 8]', () => {
+		expect(sorter.sort([8, 4, 6, 2, 3, 5, 7, 1], 0, 7)).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+	})
 })
 
-test('sorts input [1, 2] to output [1, 2]', () => {
-	expect(sort([1, 2], 0, 1)).toEqual([1, 2]);
-})
-
-test('sorts input [2, 1] to output [1, 2]', () => {
-	expect(sort([2, 1], 0, 1)).toEqual([1, 2]);
-})
-
-test('sorts input [3, 2, 1] to output [1, 2, 3]', () => {
-	expect(sort([3, 2, 1], 0, 2)).toEqual([1, 2, 3]);
-})
-
-test('sorts input [1, 2, 3] to output [1, 2, 3]', () => {
-	expect(sort([1, 2, 3], 0, 2)).toEqual([1, 2, 3]);
-})
-
-test('sorts input [1, 2, 3, 4] to output [1, 2, 3, 4]', () => {
-	expect(sort([1, 2, 3, 4], 0, 3)).toEqual([1, 2, 3, 4]);
-})
-
-test('sorts input [2, 4, 1, 3] to output [1, 2, 3, 4]', () => {
-	expect(sort([2, 4, 1, 3], 0, 3)).toEqual([1, 2, 3, 4]);
-})
-
-test('sorts input [4, 3, 2, 1] to output [1, 2, 3, 4]', () => {
-	expect(sort([4, 3, 2, 1], 0, 3)).toEqual([1, 2, 3, 4]);
-})
-
-test('sorts input [8, 4, 6, 2, 3, 5, 7, 1 to output [1, 2, 3, 4, 5, 6, 7, 8]', () => {
-	expect(sort([8, 4, 6, 2, 3, 5, 7, 1], 0, 7)).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
-})
 
