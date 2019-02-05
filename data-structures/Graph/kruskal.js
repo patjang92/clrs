694ed3d12bc2166481ae0d8f722b0aba14463134
Patrick Jang <patrick.jang@dynamo.works>
2019-02-04 19:44:40 -0500

    cleanup

--- data-structures/Graph/kruskal.js
+++ data-structures/Graph/kruskal.js
@@ -2,7 +2,6 @@ import Graph from './Graph';
 import DisjointSet from '../DisjointSet/DisjointSet';
 import QuickSort from '../../algorithms/sorts/QuickSort';
 
-
 export default function kruskal(graph) {
 
   if (graph.isDirected) throw new Error("Kruskal's algorithm only works on undirected graphs");
@@ -23,30 +22,15 @@ export default function kruskal(graph) {
   }
 
   const sortedEdges = new QuickSort(callbacks).sort(graph.getAllEdges())
-  console.log("sortedEdges = ", sortedEdges);
 
   sortedEdges.forEach((edge) => {
     const { startVertex, endVertex } = edge;
-    console.log("\n---------------------------new iteration------------------------------")
-    // console.log('edge :', edge);
-    console.log('startVertex.value :', startVertex.value);
-    console.log('endVertex.value :', endVertex.value);
-    console.log('disjointedSet.items :', Object.values(disjointSet.items).map(item => { return { key: item.getKey(), root: item.getRoot().getKey() }}));
-    const startVertexSet = disjointSet.find(startVertex);
-    const endVertexSet = disjointSet.find(endVertex);
-    console.log("startVertexSet = ", startVertexSet);
-    console.log("endVertexSet = ", endVertexSet);
 
     if (!disjointSet.inSameSet(startVertex, endVertex)) {
-      console.log("adding edge");
       mst.addEdge(edge);
-      console.log('mst.getAllEdges() :', mst.getAllEdges());
       disjointSet.union(startVertex, endVertex);
-      // console.log('disjointedSet.items :', Object.values(disjointSet.items));
-
     }
   })
-  console.log("ACTUAL edges = ", mst.getAllEdges().map(({startVertex, endVertex}) => { return { startVertex, endVertex }}))
 
   return mst;
 }
